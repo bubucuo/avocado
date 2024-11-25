2b4e3bb249ad924db501df562cd8f90c476bd2cd
bubucuo <276185876@qq.com>
2024-11-25 14:41:03 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import AntdFormPage from "./pages/AntdFormPage";
+
+import MyRCFieldForm from "./pages/MyRCFieldForm";
+
+export default function App(props) {
+  return (
+    <div>
+      {/* <AntdFormPage /> */}
+      <MyRCFieldForm />
+    </div>
+  );
+}
